99bb7a7020e1df688ab5640b04f936010855d730
JwuCode <jerrywu2390@gmail.com>
2022-11-20 20:02:11 -0500

    Added LED color changing depending on sensor value

--- airqualitymonitor.cpp
+++ airqualitymonitor.cpp
@@ -1,73 +1,96 @@
-/* Test sketch for Adafruit PM2.5 sensor with UART or I2C */
-
 #include "Adafruit_PM25AQI.h"
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// If your PM2.5 is UART only, for UNO and others (without hardware serial) 
-// we must use software serial...
-// pin #2 is IN from sensor (TX pin on sensor), leave pin #3 disconnected
-// comment these two lines if using hardware serial
-//#include <SoftwareSerial.h>
-//SoftwareSerial pmSerial(2, 3);
+#define WIRE Wire
 
+SoftwareSerial pmSerial(2, 3);
 Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &WIRE);
 
-void setup() {
-  // Wait for serial monitor to open
+int LED1 = 3;
+int LED2 = 6;
+int LED3 = 9;
+
+void setup()
+{
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
   Serial.begin(115200);
-  while (!Serial) delay(10);
+  while (!Serial)
+    delay(10);
 
-  Serial.println("Adafruit PMSA003I Air Quality Sensor");
-
-  // Wait one second for sensor to boot up!
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
   delay(1000);
 
-  // If using serial, initialize it and set baudrate before starting!
-  // Uncomment one of the following
-  //Serial1.begin(9600);
-  //pmSerial.begin(9600);
+  pmSerial.begin(9600);
 
-  // There are 3 options for connectivity!
-  if (! aqi.begin_I2C()) {      // connect to the sensor over I2C
-  //if (! aqi.begin_UART(&Serial1)) { // connect to the sensor over hardware serial
-  //if (! aqi.begin_UART(&pmSerial)) { // connect to the sensor over software serial 
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+
+  if (!aqi.begin_UART(&pmSerial))
+  {
     Serial.println("Could not find PM 2.5 sensor!");
-    while (1) delay(10);
+    while (1)
+      delay(10);
   }
 
   Serial.println("PM25 found!");
 }
 
-void loop() {
+void loop()
+{
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Air Quality Monitor\n\n");
+  display.display();
+
   PM25_AQI_Data data;
-  
-  if (! aqi.read(&data)) {
-    Serial.println("Could not read from AQI");
-    delay(500);  // try again in a bit!
+
+  /*if (! aqi.read(&data)) {
+    Serial.println("Could not read from AQI\n");
+    delay(500);
     return;
   }
-  Serial.println("AQI reading success");
+  Serial.println("AQI reading success");*/
 
-  Serial.println();
-  Serial.println(F("---------------------------------------"));
-  Serial.println(F("Concentration Units (standard)"));
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
-  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
-  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
-  Serial.println(F("Concentration Units (environmental)"));
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_env);
-  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_env);
-  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_env);
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("Particles > 0.3um / 0.1L air:")); Serial.println(data.particles_03um);
-  Serial.print(F("Particles > 0.5um / 0.1L air:")); Serial.println(data.particles_05um);
-  Serial.print(F("Particles > 1.0um / 0.1L air:")); Serial.println(data.particles_10um);
-  Serial.print(F("Particles > 2.5um / 0.1L air:")); Serial.println(data.particles_25um);
-  Serial.print(F("Particles > 5.0um / 0.1L air:")); Serial.println(data.particles_50um);
-  Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
-  Serial.println(F("---------------------------------------"));
-  
+  data.pm25_standard = 50;
+
+  Serial.print(F("PM 2.5: "));
+  Serial.print(data.pm25_standard);
+  Serial.print(" ug/m3\n");
+  display.print("PM 2.5: ");
+  display.print(data.pm25_standard);
+  display.print(" ug/m3\n");
+  display.display();
+
+  if (data.pm25_standard <= 12)
+  {
+    digitalWrite(LED1, HIGH);
+    delay(1000);
+    digitalWrite(LED1, LOW);
+  }
+  else if (data.pm25_standard > 12 && data.pm25_standard < 35)
+  {
+    digitalWrite(LED2, HIGH);
+    delay(1000);
+    digitalWrite(LED2, LOW);
+  }
+  else
+  {
+    digitalWrite(LED3, HIGH);
+    delay(1000);
+    digitalWrite(LED3, LOW);
+  }
 
   delay(1000);
 }
